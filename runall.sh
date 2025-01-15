96e7900f803c8242bb617f8f9593e617ad72a327
Eni Bundo <eni.bundo@gmail.com>
2025-01-15 21:44:21 +0100

    Add lightbulb emoji on a second paragraph

--- runall.sh
+++ runall.sh
@@ -3,16 +3,16 @@
 cd service-binance
 npm run ci
 npm run build
-cd ../ 
+cd .. 
 
 cd service-kraken
 npm run ci
 npm run build
-cd ../
+cd ..
 
 cd service-api
 npm run ci
 npm run build
-cd ../
+cd ..
 
 docker compose build && docker compose up
